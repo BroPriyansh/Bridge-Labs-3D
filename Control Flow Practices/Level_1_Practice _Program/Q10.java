4e28be00c3ab52189f84fa1fcd7ca97cc50228d1
BroPriyansh <priyanshtyagi30@gmail.com>
2025-08-23 22:46:11 +0530

    Level 1 Practice programs Solved

---
+++ Control Flow Practices/Level_1_Practice _Program/Q10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Q10 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double total = 0.0;
+    while (true){
+      double x = sc.nextDouble();
+      if (x == 0.0) break;
+      total += x;
+    }
+    System.out.println(total);
+  }
+}
